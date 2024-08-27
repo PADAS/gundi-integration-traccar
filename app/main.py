6d4696406e644612b092968ba2f1278fb73467e0
Victor H. García <victorhugogarcia13d@gmail.com>
2024-08-27 16:23:50 -0600

    Fix typo

--- app/main.py
+++ app/main.py
@@ -81,7 +81,7 @@ async def execute(
     json_payload = json.loads(payload)
     print(f"JSON Payload: {json_payload}")
     if settings.PROCESS_PUBSUB_MESSAGES_IN_BACKGROUND:
-        print(f"Running synchronously...")
+        print(f"Running in background...")
         background_tasks.add_task(
             execute_action,
             integration_id=json_payload.get("integration_id"),
